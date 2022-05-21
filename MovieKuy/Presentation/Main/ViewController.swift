455831361b1e893e3c1cb68d4fb816c56181993c
raden-dimas012 <dimascode012@gmail.com>
2022-05-21 19:57:57 +0700

    Init project and setup dependency manager with cocoapods and firebase library for auth.

---
+++ MovieKuy/Presentation/Main/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieKuy
+//
+//  Created by Raden Dimas on 21/05/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .blue
+    }
+
+
+}
+
